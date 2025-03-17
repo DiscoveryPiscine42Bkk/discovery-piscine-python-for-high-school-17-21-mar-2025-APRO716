ff93396193f1896831b642fb30dad634768592bb
APRO716 <apro71600@gmail.com>
2025-03-17 16:44:44 +0700

    mafs

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+Num1 = int(input("Enter the first number: "))
+Num2 = int(input("Enter the second number: "))
+
+print(str(Num1) + " x " + str(Num2) + " =" , str(Num1*Num2))
