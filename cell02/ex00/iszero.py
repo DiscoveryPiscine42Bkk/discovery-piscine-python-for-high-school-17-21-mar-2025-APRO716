107e3a15a1226d79061aa83e3b6a31e9f139867b
APRO716 <apro71600@gmail.com>
2025-03-17 16:17:37 +0700

    2.0

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+Number = int(input("Enter a number: "))
+
+if Number != 0:
+    print("This number is different from zero.")
+else:
+    print("This number is equal to zero.")
