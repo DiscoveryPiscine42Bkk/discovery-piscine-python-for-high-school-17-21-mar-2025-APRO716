5a350767c9b0b349e8692dc91bcea8aa5f5f6e22
APRO716 <apro71600@gmail.com>
2025-03-17 16:47:57 +0700

    bruv

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -2,5 +2,13 @@
 
 Num1 = int(input("Enter the first number: "))
 Num2 = int(input("Enter the second number: "))
+Result = Num1*Num2
 
-print(str(Num1) + " x " + str(Num2) + " =" , str(Num1*Num2))
+print(str(Num1) + " x " + str(Num2) + " =" , Result)
+
+if Result < 0:
+    print("The result is negative.")
+elif Result == 0:
+    print("The result is both positve and negative.")
+else:
+    print("The result is positve.")
