5f60c8c4afb3778551f7869fb9c12f13a78ce4a3
APRO716 <apro71600@gmail.com>
2025-03-18 11:40:05 +0700

    weeeeee

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,7 +1,7 @@
 print("Give me the first number: ")
-num1 = int(input())
+num1 = float(input())
 print("Give me the second number: ")
-num2 = int(input())
+num2 = float(input())
 print("Thank You!")
 try:
     print(f"{num1} + {num2} = {num1 + num2}")
@@ -11,4 +11,4 @@ try:
 
 except ZeroDivisionError:
     print(f"{num1} / {num2} = Undefined") # / 0 is not allowed & fix ZeroDivisionError error
-    print(f"{num1} x {num2} = {num1 * num2}") # add ts again to keep running till the end ts pmo :broken_heart:
+    print(f"{num1} x {num2} = {num1 * num2}") # add ts again to keep running till the end ts pmo :broken_heart:6
---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,12 @@
+user_input = input("Enter a string: ")
+
+swapped_string = ""
+for char in user_input:
+    if char.islower():
+        swapped_string += char.upper()
+    elif char.isupper():
+        swapped_string += char.lower()
+    else:
+        swapped_string += char  #wth
+
+print(swapped_string)
